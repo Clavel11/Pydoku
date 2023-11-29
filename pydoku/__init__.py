8bc11782b68dc4c76adb7b422bf51bffde21b2a3
Clavel11 <141845531+Clavel11@users.noreply.github.com>
2023-11-29 15:55:44 -0600

    Update __init__.py
    Se agregó la función que modifica la matriz de ceros a la matriz sudoku

--- pydoku/__init__.py
+++ pydoku/__init__.py
@@ -1,3 +1,7 @@
 import numpy as np
-mat_sudoku = np.zeros((9, 9), dtype=int)
-mat_sudoku
+
+def matrix_sudoku(index_row, index_column, digit):
+    mat_sudoku = np.zeros((9, 9), dtype=int)
+    for i in range(len(digit)):
+        mat_sudoku[index_row[i]][index_column[i]] = digit[i]
+    return mat_sudoku
