8bbc1b0f578ab5179500645bcf0a0320be245a50
brenda31416 <141837475+brenda31416@users.noreply.github.com>
2023-11-29 15:34:21 -0600

    matriz __init__.py
    Se agregó la función para la matriz del sudoku

--- pydoku/__init__.py
+++ pydoku/__init__.py
@@ -1,23 +1,3 @@
-def my_sum(x, y):
-    """A function that sums. """
-    return x+y
-
-
-def my_mul(x, y):
-    """A function that multiply. """
-    return x*y
-
-def my_rest(x, y):
-    """A function that multiply. """
-    return x-y
-
-def suma_alternada(n):
-    suma = 0
-    for i in range(1, n + 1):
-        if i % 2 != 0:
-            #impares
-            suma = suma + i**2
-        else:
-            #pares
-            suma = suma - i**2
-    return suma
+import numpy as np
+mat_sudoku = np.zeros((6, 6), dtype=int)
+mat_sudoku
