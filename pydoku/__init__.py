28e9136139ebe29e1e80e66c1ae5c0438aef4522
brenda31416 <141837475+brenda31416@users.noreply.github.com>
2023-11-30 12:41:24 -0600

    Update __init__.py

--- pydoku/__init__.py
+++ pydoku/__init__.py
@@ -1,7 +1,6 @@
 import numpy as np
-
+mat_sudoku = np.zeros((9, 9), dtype=int)
 def matrix_sudoku(index_row, index_column, digit):
-    mat_sudoku = np.zeros((9, 9), dtype=int)
     for i in range(len(digit)):
         mat_sudoku[index_row[i]][index_column[i]] = digit[i]
     return mat_sudoku
