1a890cbf3b7c03e195bac38a0a8fcd1c467b74a1
Clavel11 <141845531+Clavel11@users.noreply.github.com>
2023-12-03 12:41:26 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+"""Setup for package pydoku"""
+
 from setuptools import setup
 
 setup(name='Pydoku',
@@ -6,6 +8,5 @@ setup(name='Pydoku',
       url='https://github.com/Clavel11/Pydoku',
       author='Brenda Escorcia, Clavel Nolasco',
       author_email='es353611@uaeh.edu.mx, no420760@uaeh.edu.mx',
-      license='MIT',
       packages=['pydoku'],
       zip_safe=False)
