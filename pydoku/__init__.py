4344a3708c3432544c2a1df0bd13302ebc999c73
Clavel11 <141845531+Clavel11@users.noreply.github.com>
2023-12-03 11:36:55 -0600

    Update __init__.py

--- pydoku/__init__.py
+++ pydoku/__init__.py
@@ -1,8 +1,3 @@
-import numpy as np
-mat_sudoku = np.zeros((9, 9), dtype=int)
-def matrix_sudoku(index_row, index_column, digit):
-    for i in range(len(digit)):
-        mat_sudoku[index_row[i]][index_column[i]] = digit[i]
-    return mat_sudoku
+"""Initialization for pydoku package"""
 
-from pydoku import pre_digits
+from pydoku.matrix import *
