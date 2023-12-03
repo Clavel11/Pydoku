018ba0c1cb990b2c021d61a90124a888e04100f2
Clavel11 <141845531+Clavel11@users.noreply.github.com>
2023-12-03 12:30:07 -0600

    Update row.py

--- pydoku/row.py
+++ pydoku/row.py
@@ -1,7 +1,8 @@
 """Function that determines which values within a row are non-zero"""
 
 def search_in_row(M, index_row):
+    op_row = []
     for i in range(9):
         if M[index_row][i] != 0:
-            op_row = M[index_row][i]
+            op_row.append(M[index_row][i])
     return op_row
