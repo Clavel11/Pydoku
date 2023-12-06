11e4977e892f74ae6f63ef6e88bc3e4595fd4fd2
Clavel11 <141845531+Clavel11@users.noreply.github.com>
2023-12-06 14:10:26 -0600

    Update matrix.py

--- pydoku/matrix.py
+++ pydoku/matrix.py
@@ -2,16 +2,10 @@
 
 import numpy as np
 
-mat_sudoku = np.zeros((9, 9), dtype=int)
+mat_sudoku = np.zeros((4, 4), dtype=int)
 
 def matrix_sudoku(index_row, index_column, digit):
     for i in range(len(digit)):
         mat_sudoku[index_row[i]][index_column[i]] = digit[i]
     return mat_sudoku
 
-mat_sudoku_4x4 = np.zeros((4, 4), dtype=int)
-
-def matrix_sudoku_4x4(index_row, index_column, digit):
-    for i in range(len(digit)):
-        mat_sudoku_4x4[index_row[i]][index_column[i]] = digit[i]
-    return mat_sudoku_4x4
