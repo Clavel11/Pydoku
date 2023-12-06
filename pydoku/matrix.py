0b325b5e44cbecc665f18b4fd4c9bbd5bf2c45f5
Clavel11 <141845531+Clavel11@users.noreply.github.com>
2023-12-06 14:17:02 -0600

    Update matrix.py

--- pydoku/matrix.py
+++ pydoku/matrix.py
@@ -1,10 +1,9 @@
-"""Function that becomes a matrix full of zeros to a sudoku matrix"""
-
 import numpy as np
 
 mat_sudoku = np.zeros((4, 4), dtype=int)
 
 def matrix_sudoku(index_row, index_column, digit):
+    """Function that becomes a matrix full of zeros to a sudoku matrix"""
     for i in range(len(digit)):
         mat_sudoku[index_row[i]][index_column[i]] = digit[i]
     return mat_sudoku
