4e980c6a11e2c49f0fbb363ac4a90bf326fed781
Clavel11 <141845531+Clavel11@users.noreply.github.com>
2023-12-10 20:57:35 -0600

    Delete pydoku/column.py no se ocupa

--- pydoku/column.py
+++
@@ -1,9 +0,0 @@
-"""Functions for columns"""
-
-def options_column(matrix_s, index_column):
-    """Function that determines the options within a column"""
-    options = [1,2,3,4]
-    for digit in matrix_s[:,index_column]:
-        if digit !=0:
-            options.remove(digit)
-    return options
