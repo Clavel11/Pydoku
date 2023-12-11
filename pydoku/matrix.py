5ccb10267745df3cd5bd0b7cca31713f8202b7e1
brenda31416 <141837475+brenda31416@users.noreply.github.com>
2023-12-10 18:21:15 -0600

    Update matrix.py

--- pydoku/matrix.py
+++ pydoku/matrix.py
@@ -1,10 +1,17 @@
 import numpy as np
-
-mat_sudoku = np.zeros((4, 4), dtype=int)
+import itertools
 
 def matrix_sudoku(index_row, index_column, digit):
-    """Function that becomes a matrix full of zeros to a sudoku matrix"""
+    """Function to fill the predefined cells of a 9x9 matrix to a sudoku problem""""
+    global mat_s
+    print("Here is the Sudoku Problem :")
     for i in range(len(digit)):
-        mat_sudoku[index_row[i]][index_column[i]] = digit[i]
-    return mat_sudoku
+        mat_s[index_row[i]][index_column[i]] = digit[i]
+    printMatrix()
+
+def printMatrix():
+    """Function to print a matrix by row"""
+    global mat_s
+    for row in mat_s:
+        print(row)
 
