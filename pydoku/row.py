f2879ef68b1cede4e61d1e0dd8e8782247cb1522
brenda31416 <141837475+brenda31416@users.noreply.github.com>
2023-12-10 18:23:56 -0600

    Update row.py

--- pydoku/row.py
+++ pydoku/row.py
@@ -1,9 +1,11 @@
-"""Functions for rows"""
+"""Function for rows"""
 
-def options_row_4x4(matrix_s, index_row):
-    """Function that determines which values within a row are non-zero"""
-    options = [1,2,3,4]
-    for digit in matrix_s[index_row]:
+def options_row(index_row):
+    """Function to search digits tha are not in a specific row"""
+    global mat_s
+    options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for digit in mat_s[index_row]:
         if digit !=0:
             options.remove(digit)
     return options
+
