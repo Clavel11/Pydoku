a6a5385223c7c61368e2317043b2861b2d918b96
Clavel11 <141845531+Clavel11@users.noreply.github.com>
2023-12-11 10:45:57 -0600

    Update matrix.py

--- pydoku/matrix.py
+++ pydoku/matrix.py
@@ -5,7 +5,7 @@ mat_s = np.zeros((9, 9), dtype=int)
 def matrix_sudoku(index_row, index_column, digit):
     """Function to fill the predefined cells of a 9x9 matrix to a sudoku problem"""
     global mat_s
-    print("Here is the Sudoku Problem :")
+    print("Here is the Sudoku Problem:")
     for i in range(len(digit)):
         mat_s[index_row[i]][index_column[i]] = digit[i]
     printMatrix()
