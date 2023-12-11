76bd989f8b8d5fd0e6f2c6f3129d9f0995f50a53
brenda31416 <141837475+brenda31416@users.noreply.github.com>
2023-12-10 18:29:59 -0600

    Update row.py

--- pydoku/row.py
+++ pydoku/row.py
@@ -1,7 +1,7 @@
 """Function for rows"""
 
 def options_row(index_row):
-    """Function to search digits tha are not in a specific row"""
+    """Function to search digits that are not in a specific row"""
     global mat_s
     options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     for digit in mat_s[index_row]:
