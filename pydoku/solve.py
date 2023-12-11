d78ad1e3c61730c9dae29b609ea2ef43adb9a243
Clavel11 <141845531+Clavel11@users.noreply.github.com>
2023-12-10 22:18:26 -0600

    mejor lo quito, salieron más errores

--- pydoku/solve.py
+++ pydoku/solve.py
@@ -1,7 +1,6 @@
 """Functions to solve a sudoku"""
 
 import itertools
-from pydoku.row import options_row
 
 def checkValidFunc(row, column, num):
     """Auxiliary function to check if the digit to be assigned is not repeated in the column and in the 3x3 submatrix"""
